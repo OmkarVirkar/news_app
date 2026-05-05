c0734488fe973bfeb430f41306a03be502546f61
Omkar Virkar <iamomkarvirkar@gmail.com>
2026-05-05 09:49:41 +0530

    Impliments client side fetching for main News list page

--- app/(content)/news/page.jsx
+++ app/(content)/news/page.jsx
@@ -1,12 +1,46 @@
-import Link from "next/link";
+"use client";
+
 import { DUMMY_NEWS } from "@/assets/dummy-news";
 import NewsList from "@/components/news-list";
+import { useEffect, useState } from "react";
 
 export default function Page() {
+  const [errorState, setErrorState] = useState(null);
+  const [news, setNews] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchNews() {
+      try {
+        const response = await fetch("http://localhost:8080/news");
+        if (!response.ok) {
+          setErrorState("Failed to fetch news");
+        } else {
+          const data = await response.json();
+          setNews(data);
+        }
+        setLoading(false);
+      } catch (error) {
+        setErrorState("Error fetching news: " + error.message);
+        setLoading(false);
+      }
+    }
+
+    fetchNews();
+  }, []);
+
+  if (loading) {
+    return <p>Loading news...</p>;
+  }
+
+  if (errorState) {
+    return <p>{errorState}</p>;
+  }
+
   return (
     <>
       <h1>News Page</h1>
-      <NewsList news={DUMMY_NEWS} />
+      <NewsList news={news} />
     </>
   );
 }
--- app/(marketing)/layout.js
+++ app/(marketing)/layout.js
@@ -1,10 +1,3 @@
-import "../globals.css";
-
-export const metadata = {
-  title: "Next.js Page Routing & Rendering",
-  description: "Learn how to route to different pages.",
-};
-
 export default function MarketingLayout({ children }) {
   return <main>{children}</main>;
 }
